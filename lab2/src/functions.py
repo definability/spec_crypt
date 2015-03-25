db8bfdb93ec171a0cf5ebb187dde88a77770f468
char-lie <webmaster976@gmail.com>
2015-03-25 23:41:53 +0200

    Prettified

--- lab2/src/functions.py
+++ lab2/src/functions.py
@@ -5,12 +5,9 @@ def pow_mod_oneliner(a, x, p):
   return reduce(lambda x, y: (x*x*(a if y else 1)) % p, [int(d) for d in bin(x)[2:]], 1)
 
 def pow_mod_simple(a, x, p):
-  a %= p
-  sqr_mod = lambda y: mul_mod(x, x, p)
-  pow_mod_bin = lambda y, b: (y if b else 1)
+  pow_bin = lambda y, b: (y if b else 1)
   bit_x = [int(digit) for digit in bin(x)[2:]]
-  result = reduce(lambda x, y: mul_mod(sqr_mod(x), pow_mod_bin(a, y)), bit_x, 1)
-  return result
+  return reduce(lambda x, y: mul_mod(x*x, pow_mod_bin(a, y)), bit_x, 1)
 
 pow_mod = pow_mod_simple
 
