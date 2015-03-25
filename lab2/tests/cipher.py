9ff604152cdcc94615ec3f22fd739b7e7e3f3fb2
char-lie <webmaster976@gmail.com>
2015-03-26 00:35:24 +0200

    Prettified test

--- lab2/tests/cipher.py
+++ lab2/tests/cipher.py
@@ -16,8 +16,10 @@ class TestFunctions(TestCase):
     self.assertEqual(ROL(1, 0), 1)
     self.assertEqual(ROL(1, 32), 1)
     self.assertEqual(ROL(1, 64), 1)
-    self.assertEqual(ROL(int('1'*4 + '0'*28, 2), 2), int('1'*2 + '0'*28 + '1'*2, 2))
-    self.assertEqual(bin(ROL(int('1'*4 + '0'*28, 2), 3)), bin(int('1' + '0'*28 + '1'*3, 2)))
+    self.assertEqual(ROL(int('1'*4 + '0'*28, 2), 2),
+                         int('1'*2 + '0'*28 + '1'*2, 2))
+    self.assertEqual(ROL(int('1'*4 + '0'*28, 2), 3),
+                         int('1' + '0'*28 + '1'*3, 2))
 
 
 if __name__ == '__main__':
